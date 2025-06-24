4a7615ae26fe85547da0a478e80a074a9173ca95
Danilo Costa <p.danilo.98@hotmail.com>
2025-06-24 12:40:36 -0300

    Ajustes no estilo e efeitos de hover. Cor e gradiente baseadas no tipo.

--- src/app/pages/pokemon-list/pokemon-list.page.ts
+++ src/app/pages/pokemon-list/pokemon-list.page.ts
@@ -1,8 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { PokemonService } from '../../services/pokemon.service';
-
+import { ViewChild } from '@angular/core';
+import { IonContent } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
+import {
+  trigger,
+  transition,
+  style,
+  animate,
+  query,
+  stagger
+} from '@angular/animations';
 
 @Component({
   selector: 'app-pokemon-list',
@@ -10,11 +19,30 @@ import { IonicModule } from '@ionic/angular';
   styleUrls: ['./pokemon-list.page.scss'],
   standalone: true,
   imports: [IonicModule, CommonModule],
+  animations: [
+    trigger('cardListAnimation', [
+      transition(':enter', [
+        query(
+          '.pokemon-card',
+          [
+            style({ opacity: 0, transform: 'translateY(20px)' }),
+            stagger(100, [
+              animate(
+                '400ms ease-out',
+                style({ opacity: 1, transform: 'translateY(0)' })
+              )
+            ])
+          ],
+          { optional: true }
+        )
+      ])
+    ])
+  ]
 })
 export class PokemonListPage implements OnInit {
-
+  @ViewChild('contentRef', { static: false }) content!: IonContent;
   pokemons: any[] = [];
-  limit: number = 20;
+  limit: number = 24;
   offset: number = 0;
 
   constructor(private pokemonService: PokemonService) { }
@@ -24,10 +52,23 @@ export class PokemonListPage implements OnInit {
   }
 
   loadPokemons() {
-    this.pokemonService.getPokemonList(this.limit, this.offset).subscribe(response => {
-      this.pokemons = response.results;
-    });
-  }
+  this.pokemonService.getPokemonList(this.limit, this.offset).subscribe(async response => {
+    const results = response.results;
+
+    this.pokemons = await Promise.all(
+      results.map(async (pokemon: any) => {
+        const details = await this.pokemonService.getPokemonDetails(pokemon.name).toPromise();
+        return {
+          ...pokemon,
+          types: details.types.map((t: any) => t.type.name)
+        };
+      })
+    );
+    setTimeout(() => {
+    this.content?.scrollToTop(300);
+    }, 0);
+  });
+}
 
   getPokemonId(url: string): number {
     const parts = url.split('/');
@@ -44,5 +85,44 @@ export class PokemonListPage implements OnInit {
       this.offset -= this.limit;
       this.loadPokemons();
     }
+  }
+
+  onPokemonSelected(pokemon: any) {
+    console.log('Selecionado:', pokemon.name);
+  }
+
+  getCardGradient(pokemon: any): string {
+    const types = pokemon.types;
+    if (!types || types.length === 0) return 'background: #ccc;';
+
+    const color1 = this.getTypeColor(types[0]);
+    const color2 = types[1] ? this.getTypeColor(types[1]) : color1;
+
+    return `background: linear-gradient(135deg, ${color1}, ${color2});`;
+  }
+
+  getTypeColor(type: string): string {
+  const typeColors: Record<string, string> = {
+    fire: '#f08030',
+    water: '#6890f0',
+    grass: '#78c850',
+    electric: '#f8d030',
+    psychic: '#f85888',
+    ice: '#98d8d8',
+    ground: '#e0c068',
+    rock: '#b8a038',
+    ghost: '#705898',
+    dark: '#705848',
+    steel: '#b8b8d0',
+    fairy: '#ee99ac',
+    dragon: '#7038f8',
+    bug: '#a8b820',
+    fighting: '#c03028',
+    poison: '#a040a0',
+    normal: '#a8a878',
+    flying: '#a890f0'
+  };
+
+    return typeColors[type] || '#ccc';
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
-
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { importProvidersFrom } from '@angular/core';
@@ -12,6 +12,7 @@ bootstrapApplication(AppComponent, {
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideHttpClient(),
+    provideAnimations(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
   ],
 });
